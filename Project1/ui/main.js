af762a8c0f7443497a7d5ce9348126bd9e22ac4c
Kshitij Dhar <billdhar@gmail.com>
2017-09-19 01:46:18 +0530

    Add files via upload

---
+++ Project1/ui/main.js
@@ -0,0 +1,10 @@
+console.log('Loaded!');
+
+var ele = document.getElementById("index1Para");
+
+ele.onclick = function() {
+	ele.innerHTML = "<br> <p>What's up???</p>";
+	alert('Hello There!');
+};
+
+
